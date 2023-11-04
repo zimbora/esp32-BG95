fd5028b8e611ad19fe7697bbb48c7cf854d578d8
Lucas <lucas.ua.eet@gmail.com>
2023-11-04 21:20:26 +0000

    Update editable_macros.h

--- src/editable_macros.h
+++ src/editable_macros.h
@@ -2,24 +2,24 @@
 //#define DEBUG_BG95 // define it for debug
 //#define DEBUG_BG95_HIGH // define it for high debug
 
-#ifndef
+#ifndef MAX_CONNECTIONS
 #define MAX_CONNECTIONS				  4
 #endif
-#ifndef
+#ifndef MAX_TCP_CONNECTIONS
 #define MAX_TCP_CONNECTIONS			2
 #endif
-#ifndef
+#ifndef MAX_MQTT_CONNECTIONS
 #define MAX_MQTT_CONNECTIONS		2
 #endif
-#ifndef
+#ifndef CONNECTION_BUFFER
 #define CONNECTION_BUFFER			  650 // bytes
 #endif
-#ifndef
+#ifndef CONNECTION_STATE
 #define CONNECTION_STATE			  10000 // millis
 #endif
-#ifndef
+#ifndef SMS_CHECK_INTERVAL
 #define SMS_CHECK_INTERVAL			30000 // milli
 #endif
-#ifndef
+#ifndef MQTT_RECV_MODE
 #define MQTT_RECV_MODE				  1 // 0 - for urc containing payload; 1 - to use buffers
 #endif
